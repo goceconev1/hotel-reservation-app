1708fe611c2df591f7c5351e5e112bd97befa294
goceconev <goceconev@live.com>
2024-05-23 11:34:42 +0200

    Final Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { ReservationModule } from './reservation/reservation.module';
+import { HomeModule } from './home/home.module';
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule,
+    ReservationModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
